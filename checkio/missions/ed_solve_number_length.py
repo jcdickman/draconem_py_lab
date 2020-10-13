dfdf29ade57397fd764e844f6aeba977cb3db950
elliot <elliotjkd@gmail.com>
2020-10-13 14:05:01 -0500

    completed assignment 5

---
+++ checkio/missions/ed_solve_number_length.py
@@ -0,0 +1,20 @@
+def number_length(a: int) -> int:
+    # your code here
+    # algorithm
+    # 1) Will recive an argument called "a" of datatype "int"
+    # 2) Convert the integer into a string
+    # 3) Calculate the length of the string
+    # 4) Return the Length of the string
+    return len(str(a))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(number_length(10))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert number_length(10) == 2
+    assert number_length(0) == 1
+    assert number_length(4) == 1
+    assert number_length(44) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
